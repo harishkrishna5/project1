1f0b03d981277225fb3396c40888d052d36e5d00
harishkrishna5 <harishkrish09@gmail.com>
2024-11-25 18:02:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,31 @@
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+
+
+resource "aws_eks_cluster" "eks" {
+
+  name     = var.cluster_name
+
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+
+    subnet_ids = var.subnet_ids
+
+  }
+
+}
+
+
+
+output "eks_cluster_endpoint" {
+
+  value = aws_eks_cluster.eks.endpoint
+
+}
+
 
